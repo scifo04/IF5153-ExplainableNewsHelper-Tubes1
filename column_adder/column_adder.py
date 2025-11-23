4d6332c001013fbf0cdcd909d74f00b781346d6e
Marvin Scifo <scifohutahaean@gmail.com>
2025-11-23 13:48:31 +0700

    feat: final column adder

--- column_adder/column_adder.py
+++ column_adder/column_adder.py
@@ -2,7 +2,7 @@ import json
 import pandas as pd
 import re
 
-with open("added_column_from_prompt.json", "r", encoding="utf-8") as f:
+with open("added_data_from_prompts.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 ner_json_list = data[0]
@@ -12,18 +12,15 @@ ner_list = []
 summary_list = []
 
 for ner in ner_json_list:
+    print(ner['answer'])
     ner_list.append(json.loads(ner['answer']))
 
 for i, summary in enumerate(summary_json_list):
     summary_list.append(summary['answer'])
 
-df = pd.read_csv("../scraper/investor_articles_full.csv")
-
-df["body_text"] = df["body_text"].apply(
-    lambda x: re.sub(r"^[^—–-]*[—–-]\s*", "", x)
-)
+df = pd.read_csv("../scraper/final_dataset.csv")
 
 df['ner'] = ner_list
 df['summary'] = summary_list
 
-df.to_csv('investor_articles_with_ner_and_summaries.csv', index=False)
+df.to_csv('final_processed_dataset.csv', index=False)
